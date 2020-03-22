f56cc497175dfc430fca4a8017fe6ccf0aec65b9
Borja <uo258643@uniovi.es>
2020-03-22 15:41:15 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 app.use(express.static('public'));
 
 // Variables
@@ -15,9 +18,9 @@ app.set('port', 8081);
 app.set('db', 'mongodb+srv://admin:sdi@tiendamusica-divfp.mongodb.net/test?retryWrites=true&w=majority')
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, mongo); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
-require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
+//require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 // Lanzar el servidor
 app.listen(app.get('port'), function () {
