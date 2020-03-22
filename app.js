c8157a6b2de8c9c44eff8ca5bac94d67e7dfa31c
Borja <uo258643@uniovi.es>
2020-03-22 20:24:12 +0100

    SDI-NodeJS-8.4-Subida de ficheros y gestión de colecciones

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 let express = require('express');
 let app = express();
 
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
